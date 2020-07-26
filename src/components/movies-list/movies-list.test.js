acde7663523860d706fd550db182c8e4b814cd20
Ivan <ivanprivalov@mail.ru>
2020-07-26 03:07:15 +0300

    Adding test for the new module

--- src/components/movies-list/movies-list.test.js
+++ src/components/movies-list/movies-list.test.js
@@ -1,17 +1,18 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import MoviesList from "./movies-list.jsx";
+import MoviesList, {modes} from "./movies-list.jsx";
 
 test(`MovieCard matches snapshot`, () => {
   const tree = renderer
     .create(<MoviesList
-      moviesList = {[
+      moviesList={[
         {title: `Fantastic Beasts: The Crimes of Grindelwald`, genre: `Kids & Family`, year: 2016},
         {title: `Bohemian Rhapsody`, genre: `Drama`, year: 2018},
         {title: `Macbeth`, genre: `Drama`, year: 2015},
         {title: `Aviator`, genre: `Drama`, year: 2004}
       ]}
-      onCardHover = {() => {}}
+      onCardClick={() => {}}
+      mode={{mode: modes.ALL}}
     />).toJSON;
 
   expect(tree).toMatchSnapshot();
